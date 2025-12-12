5934e582714ffcb8ab5f97e3a205a3f6944d29f0
Neha91-svg <ng941628@gmail.com>
2025-12-12 12:07:12 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://supportflow-gamma.vercel.app"],
+      "https://supportflow-git-main-neha91-svgs-projects.vercel.app"],
     credentials: true,
   })
 );
