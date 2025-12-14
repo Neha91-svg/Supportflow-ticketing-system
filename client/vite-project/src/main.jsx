9e61722e637aa37eebd3a8ead6d904520b57305c
Neha91-svg <ng941628@gmail.com>
2025-12-14 14:20:40 +0530

    added baseurl

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // Tailwind
+import "./index.css";
+import "./utils/api";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
